098f27662db27bc66a68443da6c15a8d0bf0eb37
ahmedyosri <ahmed.yossre@gmail.com>
2020-02-03 19:10:35 +0200

    *Improved physics +Added ball Textures/Materials +Added shuffling system [WIP]

---
+++ Assets/Scripts/MatchManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour
+{
+    private MatchManager instance;
+    public MatchManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            return instance;
+        }
+    }
+
+    public List<Transform> ballPositions;
+
+    public void Awake()
+    {
+        for (int i = 0; i < ballPositions.Count; i++)
+        {
+            int x = Random.Range(0, ballPositions.Count);
+            int y = Random.Range(0, ballPositions.Count);
+            Swap(ballPositions[x], ballPositions[y]);
+        }
+    }
+
+    public void Swap<T>(T a, T b)
+    {
+        T rhs = a;
+        a = b;
+        b = rhs;
+    }
+}
